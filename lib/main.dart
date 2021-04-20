e34e64c53c854bb5a3583a945423b7ff34527903
Avdhoot <avdhootu.27@gmail.com>
2021-04-20 19:49:00 +0530

    Added

---
+++ lib/main.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'quote.dart';
+import 'quote_card.dart';
+
+void main() => runApp(MaterialApp(
+  home: QuoteList(),
+));
+
+class QuoteList extends StatefulWidget {
+  @override
+  _QuoteListState createState() => _QuoteListState();
+}
+
+class _QuoteListState extends State<QuoteList> {
+
+  List<Quote> quotes = [
+    Quote(author: 'Avdhoot Ubale', text: 'bade bade, deshon mein'),
+    Quote(author: 'Avdhoot Ubale', text: 'aesi choti choti batein'),
+    Quote(author: 'Avdhoot Ubale', text: 'hoti reheti hai Senorita')
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[300],
+      appBar: AppBar(
+        title: Text('Awesome Quotes'),
+        centerTitle: true,
+        backgroundColor: Colors.redAccent,
+      ),
+      body: Column(
+        children: quotes.map((quote) => QuoteCard(
+          quote: quote,
+          delete: () {
+            setState(() {
+              quotes.remove(quote);
+            });
+          }
+        )).toList(),
+    ),
+    );
+  }
+}
+
+
